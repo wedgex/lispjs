d82803075b85ac0c4a760ebd0d01924a8aeef490
Hunter Haydel <haydh530@gmail.com>
2017-03-18 23:58:17 -0400

    Simpl REPL

---
+++ bin/repl.js
@@ -0,0 +1,31 @@
+import readline from 'readline'
+import run from '../lib/run'
+import standardEnv from '../lib/standardEnv'
+
+function toString(exp) {
+  if (Array.isArray(exp)) {
+    return `(${exp.map(x => toString(x)).join(' ')})`
+  }
+
+  return exp
+}
+
+function repl() {
+  console.log('lisp.js repl')
+  const env = { ...standardEnv }
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+
+  rl.setPrompt('> ')
+  rl.prompt()
+
+  rl.on('line', input => {
+    const val = run(input, env)
+    if (val) console.log(toString(val))
+    rl.prompt()
+  })
+}
+
+repl()
