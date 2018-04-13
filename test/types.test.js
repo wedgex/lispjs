456058338adddccda03aec94717b6b1492521bc7
Hunter Haydel <hhaydel@gmail.com>
2018-04-13 15:40:22 -0400

    Prettier

--- test/types.test.js
+++ test/types.test.js
@@ -1,32 +1,32 @@
-import { expect } from 'chai'
-import { run } from '../'
+import { expect } from "chai";
+import { run } from "../";
 
-describe('Types', () => {
-  describe('null?', () => {
-    it('returns true if empty list', () => {
-      expect(run('(null? (list))')).to.be.true
-    })
+describe("Types", () => {
+  describe("null?", () => {
+    it("returns true if empty list", () => {
+      expect(run("(null? (list))")).to.be.true;
+    });
 
-    it('returns false otherwise', () => {
-      expect(run('(null? (list 1))')).to.be.false
-    })
-  })
+    it("returns false otherwise", () => {
+      expect(run("(null? (list 1))")).to.be.false;
+    });
+  });
 
-  describe('number?', () => {
-    it('returns true for integers', () => {
-      expect(run('(number? 1)')).to.be.true
-    })
+  describe("number?", () => {
+    it("returns true for integers", () => {
+      expect(run("(number? 1)")).to.be.true;
+    });
 
-    it('returns true for floats', () => {
-      expect(run('(number? 1.0)')).to.be.true
-    })
+    it("returns true for floats", () => {
+      expect(run("(number? 1.0)")).to.be.true;
+    });
 
-    it('returns false otherwise', () => {
-      expect(run('(number? (list))')).to.be.false
-    })
-  })
+    it("returns false otherwise", () => {
+      expect(run("(number? (list))")).to.be.false;
+    });
+  });
 
-  describe('prodedure?', () => {
+  describe("prodedure?", () => {
     // TODO
-  })
-})
+  });
+});
